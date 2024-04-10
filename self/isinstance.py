c451c7c3dc66eff9e57839ace8904818f17933a0
Hyun-jong Kim <crazyknell913@gmail.com>
2024-04-10 18:12:59 +0900

    create isinstance.py

---
+++ self/isinstance.py
@@ -0,0 +1,18 @@
+class Student:
+    def study(self):
+        print('공부를 합니다.')
+
+class Teacher:
+    def teach(self):
+        return print('학생을 가르칩니다.')
+
+classroom = [Student(), Student(), Teacher(), Student(), Student()]
+
+for person in classroom:
+    if isinstance(person, Student):
+        person.study()
+    elif isinstance(person, Teacher):
+        person.teach()
+
+
+print(Teacher.teach.__annotations__)
